400e7bb3b413f5c8b1fa528c8384d050e843a7fc
endlaz <lazlovdende@hotmail.com>
2019-10-07 12:06:21 +0200

    Add server middleware and login endpoint

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 // Server
 const { app, io } = require('./server');
 
+// Middleware
+const cors = require('cors');
+const corsMiddleware = cors();
+const bodyParser = require('body-parser').json();
+app.use(corsMiddleware, bodyParser);
+
 // Database
 const db = require('./db');
+const bcrypt = require('bcrypt');
 const User = require('./models/userModel');
-db.sync()
-    .then(() => {
+db.sync({ force: true })
+    .then(async () => {
+        await User.create({
+            email: "test@test.com",
+            username: "Lazlo",
+            password: bcrypt.hashSync("123456", 10)
+        })
         console.log('Database connected and synced')
     });
+
+// Routes
+const authRouter = require('./routers/authRouter');
+app.use(authRouter);

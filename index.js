6513723fc25a5cdc75c7429d805824b4597400f0
endlaz <lazlovdende@hotmail.com>
2019-10-07 11:01:58 +0200

    Add database connection

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 // Server
 const { app, io } = require('./server');
+
+// Database
+const db = require('./db');
+db.sync()
+    .then(() => {
+        console.log('Database connected and synced')
+    });

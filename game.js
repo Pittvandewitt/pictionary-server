0d75b045562755e09b7f992f6a615be355fdb5e4
endlaz <lazlovdende@hotmail.com>
2019-10-11 15:43:57 +0200

    Change answer options

--- game.js
+++ game.js
@@ -2,20 +2,17 @@ const { app, io } = require('./server');
 
 console.log(io);
 
-const words = [
-  "word", "letter", "number", "person", "pen", "police", "people",
-  "sound", "water", "breakfast", "place", "man", "men", "woman", "women", "boy",
-  "girl", "serial killer", "Oregon Trail", "week", "month", "name", "sentence", "line", "air",
-  "land", "home", "hand", "house", "picture", "animal", "mother", "father",
-  "big foot", "sister", "world", "head", "page", "country", "question",
-  "shiba inu", "school", "plant", "food", "sun", "state", "eye", "city", "tree",
-  "farm", "story", "sea", "night", "day", "life", "north", "south", "east",
-  "west", "child", "children", "example", "paper", "music", "river", "car",
-  "Power Rangers", "feet", "book", "science", "room", "friend", "idea", "fish",
-  "mountain", "horse", "watch", "color", "face", "wood", "list", "bird",
-  "body", "fart", "family", "song", "door", "forest", "wind", "ship", "area",
-  "rock", "Captain Planet", "fire", "problem", "airplane", "top", "bottom", "king",
-  "space", "whale", "unicorn", "narwhal", "furniture", "sunset", "sunburn", "Grumpy cat", "feather", "pigeon"
+const words = ["letter", "person", "pen", "police", "people", "water", "breakfast", "boy",
+  "girl", "line", "air",
+  "land", "hand", "house", "picture", "animal", "mother", "father",
+  "head", "plant", "food", "sun", "eye", "city", "tree",
+  "sea", "north", "south", "east",
+  "west", "child",  "music", "car",
+  "feet", "book", "fish",
+  "mountain", "horse", "watch", "bird",
+  "fart", "ship", 
+  "rock", "fire",  "airplane",  "king",
+  "space", "whale", "unicorn", "Grumpy cat", "feather", "pigeon"
 ];
 
 function newWord() {

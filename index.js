6a6d75f658a4b91eadeda0ac6036a3cc1054f0e6
pittvandewitt <thomasdewitt@outlook.com>
2019-10-11 14:10:22 +0200

    Implement lobby routing

--- index.js
+++ index.js
@@ -11,16 +11,23 @@ app.use(corsMiddleware, bodyParser);
 const db = require('./db');
 const bcrypt = require('bcrypt');
 const User = require('./models/userModel');
+const Lobby = require('./models/lobbyModel');
 db.sync({ force: true })
-    .then(async () => {
-        await User.create({
-            email: "test@test.com",
-            username: "Lazlo",
-            password: bcrypt.hashSync("123456", 10)
-        })
-        console.log('Database connected and synced')
-    });
+  .then(async () => {
+    await User.create({
+      email: "test@test.com",
+      username: "Lazlo",
+      password: bcrypt.hashSync("123456", 10)
+    })
+    await Lobby.create({
+      name: 'DevRoom',
+      members: 0
+    })
+    console.log('Database connected and synced')
+  });
 
 // Routes
 const authRouter = require('./routers/authRouter');
+const lobbyRouter = require('./routers/lobbyRouter')
 app.use(authRouter);
+app.use(lobbyRouter);
--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,11 +1,14 @@
 const User = require('../models/userModel')
-const { toData } = require('./jwt')
+const { toData } = require('../auth/jwt')
 
 function auth(req, res, next) {
   const auth = req.headers.authorization && req.headers.authorization.split(' ')
+  const data = auth[1]
+  console.log('data iat', data)
   if (auth && auth[0] === 'Bearer' && auth[1]) {
     try {
       const data = toData(auth[1])
+      console.log('data iat', data)
       User
         .findByPk(data.userId)
         .then(user => {
